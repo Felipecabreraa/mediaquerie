d616fe19c93fadb9feac9865464102607aa2464c
Felipecabreraa <luislagoscabrera@gmail.com>
2024-11-24 11:45:49 -0300

    Desafio Culminado

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("hamburgerMenu").addEventListener("click", function () {
-    const sidebar = document.querySelector(".sidebar");
-    const navList = document.querySelector(".nav-list");
-  
-    sidebar.classList.toggle("open");
-    navList.classList.toggle("open");
-  });
